436a7ca8ef224fd2a17530622bf3119c95864e6d
jaehyu <goodbright0@gmail.com>
2022-04-08 19:49:01 +0900

    first commit

---
+++ src/components/Userplane.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react'
+import './Userplane.css'
+
+import { Box , Card, CardContent , Typography} from "@material-ui/core"
+
+class CourseList extends Component {
+
+    render() { 
+        return (
+            <div>
+            <img
+                src="/assets/images/escape.png"  
+                alt='testA' 
+                className="Center"
+                />
+        </div>
+        )
+    }
+}
+export default CourseList;
