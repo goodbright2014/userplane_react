099d37e2b68db837c7a7b66174c493d561793ef7
goodbright2014 <goodbright0@gmail.com>
2022-04-08 21:04:44 +0900

    a

--- src/components/Userplane.js
+++ src/components/Userplane.js
@@ -8,12 +8,93 @@ class CourseList extends Component {
     render() { 
         return (
             <div>
-            <img
-                src="/assets/images/escape.png"  
-                alt='testA' 
-                className="Center"
-                />
-        </div>
+                <Card>
+                    <CardContent>
+                
+                        <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+
+                </Card>
+
+
+                <div>
+                    <img
+                        src="/assets/images/escape.png"  
+                        alt='testA' 
+                        className="Center"
+                        />
+                </div>      
+
+                <Card>
+                    <CardContent>
+                
+                        <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+
+                </Card>
+
+                <div>
+                    <img
+                        src="/assets/images/cubic.png"  
+                        alt='testA' 
+                        className="Center"
+                        />
+                </div>
+
+                <Card>
+                    <CardContent>
+                
+                        <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+
+                </Card>
+
+                <div>
+                    <img
+                        src="/assets/images/escape.png"  
+                        alt='testA' 
+                        className="Center"
+                        />
+                </div>
+
+                <Card>
+                    <CardContent>
+                
+                        <Typography gutterBottom variant="h5" component="div">
+                        Lizard
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                        Lizards are a widespread group of squamate reptiles, with over 6,000
+                        species, ranging across all continents except Antarctica
+                        </Typography>
+                    </CardContent>
+
+                </Card>
+
+
+
+
+
+            </div>
+  
         )
     }
 }
